42e7fd115d357539054e134b1f04ca0cc3336e0f
Guilherme Silva Antunes <guilherme.silva.antunes1999@gmail.com>
2021-09-11 18:20:29 -0300

    menu settings do profile com design criado

---
+++ js/settings.js
@@ -0,0 +1,31 @@
+(function(){
+    var profile_pic = document.querySelector('nav .profile-image-nav');
+    var setting_menu = document.querySelector('.settings-menu');
+    
+    var _this = this;
+    var setting_boolean = false;
+
+    profile_pic.addEventListener('click', settingSwitcher);
+    
+    function settingSwitcher(){
+        if(!setting_boolean){
+            openMenu();
+        }
+        else{
+            closeMenu();
+        }
+    }
+    
+    function openMenu(){
+        setting_boolean = true;
+        
+        setting_menu.style.display = 'block';
+    }
+
+    function closeMenu(){
+        setting_boolean = false;
+        setting_menu.style.display = 'none';
+    }
+
+    console.log('hehe boi');
+})();
